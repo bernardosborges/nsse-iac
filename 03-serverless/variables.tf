48a81f0bc7ed5d78ad5ca0f0962fb4aa5e8a7653
bernardosborges <bernardoserpaborges@proton.me>
2026-03-20 13:37:52 -0300

    feat(serverless): setup provider, remote backend and variables

---
+++ 03-serverless/variables.tf
@@ -0,0 +1,100 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "assume_role" {
+  type = object({
+    role_arn    = string
+    external_id = string
+  })
+
+  default = {
+    role_arn    = "arn:aws:iam::583933848805:role/terraform-role"
+    external_id = "38fcec02-aedd-4633-9cca-1972705927b3"
+  }
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Environment = "production"
+    Project     = "nsse"
+  }
+}
+
+variable "queues" {
+  type = list(object({
+    name                      = string
+    delay_seconds             = number
+    max_message_size          = number
+    message_retention_seconds = number
+    receive_wait_time_seconds = number
+    sqs_managed_sse_enabled   = bool
+  }))
+
+  default = [
+    {
+      name                      = "EmailNotificationQueue"
+      delay_seconds             = 0
+      max_message_size          = 2048
+      message_retention_seconds = 86400
+      receive_wait_time_seconds = 10
+      sqs_managed_sse_enabled   = true
+    },
+    {
+      name                      = "ProductStockQueue"
+      delay_seconds             = 0
+      max_message_size          = 2048
+      message_retention_seconds = 86400
+      receive_wait_time_seconds = 10
+      sqs_managed_sse_enabled   = true
+    },
+    {
+      name                      = "InvoiceQueue"
+      delay_seconds             = 0
+      max_message_size          = 2048
+      message_retention_seconds = 86400
+      receive_wait_time_seconds = 10
+      sqs_managed_sse_enabled   = true
+    }
+  ]
+}
+
+variable "dl_queues" {
+  type = list(object({
+    name                      = string
+    delay_seconds             = number
+    max_message_size          = number
+    message_retention_seconds = number
+    receive_wait_time_seconds = number
+    sqs_managed_sse_enabled   = bool
+  }))
+
+  default = [
+    {
+      name                      = "EmailNotificationQueueDlq"
+      delay_seconds             = 0
+      max_message_size          = 2048
+      message_retention_seconds = 86400
+      receive_wait_time_seconds = 10
+      sqs_managed_sse_enabled   = true
+    },
+    {
+      name                      = "ProductStockQueueDlq"
+      delay_seconds             = 0
+      max_message_size          = 2048
+      message_retention_seconds = 86400
+      receive_wait_time_seconds = 10
+      sqs_managed_sse_enabled   = true
+    },
+    {
+      name                      = "InvoiceQueueDlq"
+      delay_seconds             = 0
+      max_message_size          = 2048
+      message_retention_seconds = 86400
+      receive_wait_time_seconds = 10
+      sqs_managed_sse_enabled   = true
+    }
+  ]
+}
