982550d7a01b0b0fb1c3723740a539adfdaa7551
bernardosborges <bernardoserpaborges@proton.me>
2026-03-23 11:50:32 -0300

    feat(rds): add s3 and rds cluster outputs

--- 03-serverless/outputs.tf
+++ 03-serverless/outputs.tf
@@ -9,3 +9,15 @@ output "queues_url" {
 output "sns_topic_arn" {
   value = aws_sns_topic.order_confirmed_topic.id
 }
+
+output "s3_application_bucket_arn" {
+  value = aws_s3_bucket.nsse.arn
+}
+
+output "rds_cluster_endpoint" {
+  value = aws_rds_cluster.this.endpoint
+}
+
+output "rds_cluster_reader_endpoint" {
+  value = aws_rds_cluster.this.reader_endpoint
+}
