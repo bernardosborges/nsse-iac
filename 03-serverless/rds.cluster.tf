751ab20170621913ddf3d30e36910ea6aedfa214
bernardosborges <bernardoserpaborges@proton.me>
2026-03-23 11:49:12 -0300

    feat(rds): setup rds aurora cluster

---
+++ 03-serverless/rds.cluster.tf
@@ -0,0 +1,23 @@
+resource "aws_rds_cluster" "this" {
+  cluster_identifier           = var.rds_aurora_cluster.cluster_identifier
+  final_snapshot_identifier    = var.rds_aurora_cluster.final_snapshot_identifier
+  engine                       = var.rds_aurora_cluster.engine
+  engine_mode                  = var.rds_aurora_cluster.engine_mode
+  database_name                = var.rds_aurora_cluster.database_name
+  master_username              = var.rds_aurora_cluster.master_username
+  preferred_maintenance_window = var.rds_aurora_cluster.preferred_maintenance_window
+  availability_zones           = var.rds_aurora_cluster.availability_zones
+  manage_master_user_password  = var.rds_aurora_cluster.manage_master_user_password
+  storage_encrypted            = var.rds_aurora_cluster.storage_encrypted
+  deletion_protection          = var.rds_aurora_cluster.deletion_protection
+  db_subnet_group_name         = aws_db_subnet_group.this.name
+  vpc_security_group_ids       = [aws_security_group.rds.id]
+
+  serverlessv2_scaling_configuration {
+    max_capacity             = var.rds_aurora_cluster.serverless_scaling_configuration.max_capacity
+    min_capacity             = var.rds_aurora_cluster.serverless_scaling_configuration.min_capacity
+    seconds_until_auto_pause = var.rds_aurora_cluster.serverless_scaling_configuration.seconds_until_auto_pause
+  }
+
+  tags = var.tags
+}
--- 03-serverless/variables.tf
+++ 03-serverless/variables.tf
@@ -148,3 +148,58 @@ variable "security_groups" {
     rds_security_group_name           = "nsse-production-rds-sg"
   }
 }
+
+variable "rds_aurora_cluster" {
+  type = object({
+    cluster_identifier           = string
+    final_snapshot_identifier    = string
+    engine                       = string
+    engine_mode                  = string
+    database_name                = string
+    master_username              = string
+    preferred_maintenance_window = string
+    availability_zones           = list(string)
+    manage_master_user_password  = bool
+    storage_encrypted            = bool
+    deletion_protection          = bool
+    instances = list(object({
+      instance_class    = string
+      identifier        = string
+      availability_zone = string
+    }))
+    serverless_scaling_configuration = object({
+      max_capacity             = number
+      min_capacity             = number
+      seconds_until_auto_pause = number
+    })
+  })
+
+  default = {
+    cluster_identifier           = "nsse-aurora-serverless-cluster"
+    final_snapshot_identifier    = "nsse-aurora-serverless-cluster-final-snapshot"
+    engine                       = "aurora-postgresql"
+    engine_mode                  = "provisioned"
+    database_name                = "notSoSimpleEcommerce"
+    master_username              = "nsseAdmin"
+    preferred_maintenance_window = "sun:05:00-sun:06:00"
+    availability_zones           = ["us-east-1a", "us-east-1b"]
+    manage_master_user_password  = true
+    storage_encrypted            = true
+    deletion_protection          = false # true in production
+    instances = [{
+      instance_class    = "db.serverless"
+      identifier        = "nsse-instance-us-east-1a"
+      availability_zone = "us-east-1a"
+      },
+      {
+        instance_class    = "db.serverless"
+        identifier        = "nsse-instance-us-east-1b"
+        availability_zone = "us-east-1b"
+    }]
+    serverless_scaling_configuration = {
+      max_capacity             = 1.0
+      min_capacity             = 0.5
+      seconds_until_auto_pause = 3600
+    }
+  }
+}
